fb46a8dd5dbc333afdf5e956f13a52fa1e0f7219
Francois Bleibel <fbleibel@gmail.com>
2017-06-12 20:57:04 -0700

    Compress

--- compress_photos.py
+++ compress_photos.py
@@ -1,12 +1,49 @@
 #!/usr/local/bin/python3
+# Read raw (cr2) files with imagemagick/convert:
 # brew install ufraw --with-exiv2
+# Videos:
+# brew install ffmpeg 
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
 import os
 import sys
+import subprocess
 
-IMAGE_CONVERT_CMD = ["convert", "{input}", "-quality 85%", "-resize 80%", "{output}"]
-VIDEO_CONVERT_CMD = ["ffmpeg", "-i", "{input}", "-quality 85%", "-resize 80%", "{output}"]
+IMAGE_CONVERT_CMD = ["convert", "{input}", "-quality", "85%", "-resize", "80%", "{output}"]
+VIDEO_CONVERT_CMD = ["ffmpeg", "-i", "{input}", "{output}"]
+
+def replace(template, input, output):
+    l = []
+    for item in template:
+        if item == "{input}":
+            l.append(input)
+        elif item == "{output}":
+            l.append(output)
+        else:
+            l.append(item)
+    return l
+    
+def convert_mov(input_path):
+    stem, _ = os.path.splitext(input_path)
+    output_path = stem + ".mp4"
+    cmd = replace(VIDEO_CONVERT_CMD, input_path, output_path)
+    print(subprocess.list2cmdline(cmd))
+    if subprocess.call(cmd) != 0:
+        print('ERROR converting', input_path)
+    else:
+        print ('remove', input_path)
+        os.remove(input_path)
+
+def convert_cr2(input_path):
+    stem, _ = os.path.splitext(input_path)
+    output_path = stem + ".jpg"
+    cmd = replace(IMAGE_CONVERT_CMD, input_path, output_path)
+    print(subprocess.list2cmdline(cmd))
+    if subprocess.call(cmd) != 0:
+        print('ERROR converting', input_path)
+    else:
+        print ('remove', input_path)
+        os.remove(input_path)
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -22,27 +59,35 @@ class MainWindow(QWidget):
         
         self.add_btn = QPushButton("Add folder...")
         self.convert_btn = QPushButton("Convert")
+        self.clear_btn = QPushButton("Clear")
         
         layout.addWidget(self.tree_widget)
-        layout.addWidget(self.add_btn)
-        layout.addWidget(self.convert_btn)
+        btn_layout = QHBoxLayout()
+        btn_layout.addWidget(self.add_btn)
+        btn_layout.addWidget(self.convert_btn)
+        btn_layout.addWidget(self.clear_btn)
+        layout.addLayout(btn_layout)
         layout.setContentsMargins(0, 0, 0, 0)
         
         self.add_btn.clicked.connect(self.add_clicked)
         self.convert_btn.clicked.connect(self.convert_clicked)
+        self.clear_btn.clicked.connect(self.clear_clicked)
+        
+    def clear_clicked(self):
+        self.tree_widget.clear()
         
     def add_clicked(self):
         dir = QFileDialog.getExistingDirectory(self, "Open directory")
         if dir:
-            self.populate_tree(dir)
+            self.add_top_level_folder(dir)
     
     def convert_clicked(self):
-        self.visit_item(self.tree_widget.invisibleRootItem())
+        self.convert_item(self.tree_widget.invisibleRootItem())
         
-    def populate_tree(self, root_dir):
+    def add_top_level_folder(self, root_dir):
         self.add_folder(self.tree_widget.invisibleRootItem(), root_dir)
         
-    def explore(self, dir):
+    def list_contents(self, dir):
         names = os.listdir(dir)
         cr2 = []
         mov = []
@@ -58,7 +103,7 @@ class MainWindow(QWidget):
         return {"cr2": cr2, "mov": mov, "dirs":dirs}
         
     def add_folder(self, parent_item, path):
-        files_dict = self.explore(path)
+        files_dict = self.list_contents(path)
         contents = [os.path.basename(path),
                     str(len(files_dict['cr2'])),
                     str(len(files_dict['mov']))]
@@ -69,11 +114,17 @@ class MainWindow(QWidget):
         for dir in files_dict['dirs']:
             self.add_folder(item, dir)
     
-    def visit_item(self, item):
-        user_data = item.data(0, QtCore.Qt.UserRole)
-        print('ud', user_data)
-        children = item.items()
-        print (children)
+    def convert_item(self, item):
+        path = item.data(0, QtCore.Qt.UserRole)
+        if path is not None:
+            contents = self.list_contents(path)
+            for movfile in contents['mov']:
+                convert_mov(movfile)
+            for cr2file in contents['cr2']:
+                convert_cr2(cr2file)
+        for i in range(item.childCount()):
+            child = item.child(i)
+            self.convert_item(child)
         
 def main():
     app = QApplication(sys.argv)
